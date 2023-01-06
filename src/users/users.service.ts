5b4772487fcf3c3b5b6eaae8103b1dbabc5fb667
Fernando Lins <fernandolins088@gmail.com>
2023-01-06 17:06:11 -0300

    feat(users): add create method and implements missing unit tests for usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Role } from '../auth/decorators/roles.decorator';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateClientDto } from './dto/create-client.dto';
 
 @Injectable()
 export class UsersService {
@@ -10,6 +12,25 @@ export class UsersService {
     return this.prisma.user.findUnique({
       where: {
         email
+      }
+    })
+  }
+
+  async create(data: CreateClientDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: data.email
+      }
+    })
+
+    if (existingUser) {
+      throw new Error('A user with the informed email already exists')
+    }
+
+    return await this.prisma.user.create({
+      data: {
+        ...data,
+        role: Role.USER
       }
     })
   }
