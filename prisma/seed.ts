b38cc1181cf0cf03e68aaefbee65b5221f668e41
Fernando Lins <fernandolins088@gmail.com>
2023-01-05 23:20:00 -0300

    feat(seed): create a seed for admin

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from ".prisma/client"
+import { hash } from 'bcrypt'
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+  const password = process.env.ADMIN_PASSWORD;
+  const hashedPassword = await hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      email: 'admin@clinica.com',
+      password: hashedPassword,
+      role: 'admin',
+    },
+  });
+}
+
+main()
