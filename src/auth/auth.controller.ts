fb601da7e9c500fb4a858981429ec90a913d878f
Fernando Lins <fernandolins088@gmail.com>
2023-01-06 05:50:35 -0300

    feat(authentication): implement passport-jwt auth strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) { }
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
@@ -14,5 +15,18 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      email: user.email,
+      sub: {
+        userId: user.userId,
+        role: user.role,
+      },
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
