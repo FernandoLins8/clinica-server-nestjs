26188506790d65d0e2ee11cae198ce33c7ae9c4f
Fernando Lins <fernandolins088@gmail.com>
2023-02-01 23:46:55 -0300

    feat(admin-seed): update admin seed to have name and email set by env variables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,13 +5,15 @@ const prisma = new PrismaClient();
 
 async function main() {
 
+  const name = process.env.ADMIN_NAME
+  const email = process.env.ADMIN_EMAIL
   const password = process.env.ADMIN_PASSWORD;
   const hashedPassword = await hash(password, 10);
 
   await prisma.user.create({
     data: {
-      name: 'Admin',
-      email: 'admin@clinica.com',
+      name,
+      email,
       password: hashedPassword,
       role: 'admin',
     },
